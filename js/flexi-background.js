2970d4e0ab8423bd9c86fd159af18104463ca1a1
Michael Bester <michael@kimili.com>
2010-10-29 12:41:42 -0400

    Removing dependency on jQuery, removing ie6 support

--- js/flexi-background.js
+++ js/flexi-background.js
@@ -10,9 +10,6 @@ About: Version
 Description:
 	Javascript to set up full-screen flexible backgrounds in all widely used browsers.
 
-Requires:
-	- jQuery 1.3.2+ <jquery.js> <http://jquery.com>
-
 License:
 	Copyright 2010, Michael Bester.
 	Released under the MIT license <http://opensource.org/licenses/mit-license.php>
@@ -29,18 +26,18 @@ License:
 		height	: 426
 	};
 	
+	/*	END CONFIGURATION */
+	
 	/*
 		Are we using a touch device?
 	*/
 	if ('ontouchstart' in window) {
-		try {
-			$('html').addClass('touch');
-		} catch(e) {}
+		document.getElementsByTagName('html').className += ' touch';
 	};
 	
 	/**
-		Detect support for CSS background-size. No need for anything else if background-size is supported.
-		Property detection adapted from Modernizr <http://modernizr.com>
+		Detect support for CSS background-size. No need for any more javascript if background-size is supported.
+		Property detection adapted from the most excellent Modernizr <http://modernizr.com>
 	*/
 	if ((function(){
 		var el 		= document.createElement('div'),
@@ -59,118 +56,177 @@ License:
 	};
 	
 	/**
-		Declare and define variables
-	*/ 
-	var $window,
-		$body,
+		If we've gotten here, we don't have background-size support,
+		so we'll have to mimic it with Javascript.
+		Let's set up some variables
+	*/
+	var elBody,
 		imageID		= 'expando',
 		tallClass	= 'tall',
 		wideClass	= 'wide',
-		$bgImage, $wrapper, img, url, imgAR,
-		
+		elBgImage, elWrapper, img, url, imgAR,
+	
 		/**
-			Are we dealing with ie6?
+			Since we're not relying on a library, we'll need some utility functions
+			First, basic cross browser event adders
 		*/
-		ie6 = ($.browser.msie && parseInt($.browser.version, 10) <= 6),
+		addEvent = function(el, evnt, func) {
+			if (el.addEventListener) {
+				el.addEventListener(evnt, func, false);
+			} else if (el.attachEvent) {
+				return el.attachEvent("on" + evnt, func);
+			} else {
+				el['on' + evnt] = func;
+			}
+		},
 	
-	/**
-		Set up the action that happens on resize
-	*/
-	resizeAction = function() {
-		var win = {
-			height : $window.height(),
-			width : $window.width()
+		domLoaded = function(callback) {
+			/* Internet Explorer */
+			/*@cc_on
+			@if (@_win32 || @_win64)
+			document.write('<script id="ieScriptLoad" defer src="//:"><\/script>');
+			document.getElementById('ieScriptLoad').onreadystatechange = function() {
+				if (this.readyState == 'complete') {
+					callback();
+				}
+			};
+			@end @*/
+			/* Mozilla, Chrome, Opera */
+			if (document.addEventListener) {
+				document.addEventListener('DOMContentLoaded', callback, false);
+			}
+			/* Safari, iCab, Konqueror */
+			if (/KHTML|WebKit|iCab/i.test(navigator.userAgent)) {
+				var DOMLoadTimer = setInterval(function () {
+					if (/loaded|complete/i.test(document.readyState)) {
+						callback();
+						clearInterval(DOMLoadTimer);
+					}
+				}, 10);
+			}
+		},
+	
+		/**
+		 	Next, a way to properly get the computed style of an element
+			Courtesy of Robert Nyman - http://robertnyman.com/2006/04/24/get-the-rendered-style-of-an-element/
+		*/
+		getStyle = function(el, css){
+			var strValue = "";
+			if (document.defaultView && document.defaultView.getComputedStyle){
+				strValue = document.defaultView.getComputedStyle(el, "").getPropertyValue(css);
+			}
+			else if (el.currentStyle){
+				css = css.replace(/\-(\w)/g, function (strMatch, p1){
+					return p1.toUpperCase();
+				});
+				strValue = el.currentStyle[css];
+			}
+			return strValue;
+		},
+	
+		/**
+			Finally, some element class manipulation functions
+		*/
+		classRegex = function(cls) {
+			return new RegExp('(\\s|^)'+cls+'(\\s|$)');
+		},
+	
+		hasClass = function(el, cls) {
+			return el.className.match(classRegex(cls));
+		},
+	
+		addClass = function(el, cls) {
+			if ( ! hasClass(el, cls)) {
+				el.className += ' ' + cls;
+			}
+		},
+	
+		removeClass = function(el, cls) {
+			if (hasClass(el, cls)) {
+				el.className = el.className.replace(classRegex(cls), '');
+			}
+		},
+	
+		/*
+			Now we can move on with the core functionality of Flexibackground
+		*/
+		initialize = function() {
+		
+			// No need for any of this if the screen isn't bigger than the background image
+			if (screen.availWidth <= bgImageSize.width && screen.availHeight <= bgImageSize.height) {
+				return;
+			}
+		
+			// Grab elements we'll reference throughout
+			elBody		= document.getElementsByTagName('body')[0];
+		
+			// Parse out the URL of the background image and drop out if we don't have one
+			url = getStyle(elBody, 'backgroundImage').replace(/^url\(("|')?|("|')?\);?$/g, '') || false;
+			if (!url || url === "none" || url === "") {
+				return;
+			}
+		
+			// Get the aspect ratio of the image
+			imgAR = bgImageSize.width / bgImageSize.height;
+
+			// Create a new image element
+			elBgImage 		= document.createElement('img');
+			elBgImage.src 	= url;
+			elBgImage.id 	= imageID;
+		
+			// Create a wrapper and append the image to it.
+			// The wrapper ensures we don't get scrollbars.
+			elWrapper = document.createElement('div');
+			elWrapper.style.overflow	= 'hidden';
+			elWrapper.style.width	= '100%';
+			elWrapper.style.height	= '100%';
+			elWrapper.style.zIndex	= '-1';
+		
+			elWrapper.appendChild(elBgImage);
+			elBody.appendChild(elWrapper);
+		
+			// Fix the wrapper into position
+			elWrapper.style.position	= 'fixed';
+			elWrapper.style.top		= 0;
+			elWrapper.style.left		= 0;
+		
+			// Set up a resize listener to add/remove classes from the body 
+			addEvent(window, 'resize', resizeAction);
+
+			// Set it up by triggering a resize
+			resizeAction();
+		
+		},
+	
+		/**
+			Set up the action that happens on resize
+		*/
+		resizeAction = function() {
+			var win = {
+				height	: window.innerHeight || document.documentElement.clientHeight,
+				width	: window.innerWidth || document.documentElement.clientWidth
+			},
+
+			// The current aspect ratio of the window
+			winAR = win.width / win.height;
+
+			// Determine if we need to show the image and whether it needs to stretch tall or wide
+			if (win.width < bgImageSize.width && win.height < bgImageSize.height) {
+				removeClass(elBody, wideClass);
+				removeClass(elBody, tallClass);
+			} else if (winAR < imgAR) {
+				removeClass(elBody, wideClass);
+				addClass(elBody, tallClass);
+				// Center the image
+				elBgImage.style.left = Math.min(((win.width - bgImageSize.width) / 2), 0);
+			} else if (winAR > imgAR) {
+				addClass(elBody, wideClass);
+				removeClass(elBody, tallClass);
+				elBgImage.style.left = 0;
+			}
 		};
-
-		// The current aspect ratio of the window
-		var winAR = win.width / win.height;
-
-		// Determine if we need to show the image and whether it needs to stretch tall or wide
-		if (win.width < bgImageSize.width && win.height < bgImageSize.height) {
-			$body
-				.removeClass(wideClass)
-				.removeClass(tallClass);
-		} else if ((win.w < bgImageSize.width && win.height >= bgImageSize.height) || (winAR < imgAR)) {
-			$body
-				.removeClass(wideClass)
-				.addClass(tallClass);
-			// Center the image
-			$bgImage.css('left', Math.min(((win.width - bgImageSize.width) / 2), 0));
-		} else if (win.width >= bgImageSize.width) {
-			$body
-				.addClass(wideClass)
-				.removeClass(tallClass);
-			$bgImage.css('left', 0);
-		}
-		
-		// Need to fix the height of the wrapper for IE6
-		if (ie6) {
-			$wrapper.css('height', win.height);
-		}
-	},
 	
-	/*
-		Sets up the basic functionality
-	*/
-	initialize = function() {
-		
-		// No need for any of this if the screen isn't bigger than the background image
-		if (screen.availWidth <= bgImageSize.width && screen.availHeight <= bgImageSize.height) {
-			return;
-		}
-		
-		// Grab elements we'll reference throughout
-		$window		= $(window);
-		$body		= $('body');
-		
-		// Parse out the URL of the background image and drop out if we don't have one
-		url = $body.css('background-image').replace(/^url\(("|')?|("|')?\);?$/g, '') || false;	
-		if (!url || url === "none" || url === "") {
-			return;
-		}
-		
-		// Get the aspect ratio of the image
-		imgAR = bgImageSize.width / bgImageSize.height;
-
-		// Create a new image element
-		$bgImage = $('<img />')
-					.attr('src', url)
-					.attr('id', imageID);
-					
-		// Create a wrapper and append the image to it.
-		// The wrapper ensures we don't get scrollbars.
-		$wrapper = $('<div></div>')
-						.css({
-							'overflow' : 'hidden',
-							'width' : '100%',
-							'height' : '100%',
-							'z-index' : '-1'
-						})
-						.append($bgImage)
-						.appendTo($body);
-						
-		// IE6 Doesn't do position: fixed, so let's fake it out.
-		// We'll apply a class which gets used in the CSS to emulate position: fixed
-		// Otherwise, we'll simply used position: fixed.
-		if (ie6) {
-			$wrapper.addClass('ie6fixed');
-		} else {
-			$wrapper.css({
-				'position' : 'fixed',
-				'top' : 0,
-				'left' : 0
-			});
-		}
-		
-		// Set up a resize listener to add/remove classes from the body 
-		$window.bind('resize', resizeAction);
-
-		// Set it up by triggering a resize
-		$window.trigger('resize');
-		
-	};
-	
-	// When the document is ready, run this thing.	
-	$(document).ready(initialize);
+	// When the document is ready, run this thing.
+	domLoaded(initialize);
 	
 })();
